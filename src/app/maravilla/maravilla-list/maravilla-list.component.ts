bc8de8c2f2a4fd3be3932fe646aa107863a5d720
Jonathan1503 <j.riverao@uniandes.edu.co>
2022-05-20 13:19:32 -0500

    Entrega final parcial 2

--- src/app/maravilla/maravilla-list/maravilla-list.component.ts
+++ src/app/maravilla/maravilla-list/maravilla-list.component.ts
@@ -10,17 +10,31 @@ import { MaravillaService } from '../maravilla.service';
 export class MaravillaListComponent implements OnInit {
 
   maravillas: Array<Maravilla>= [];
+  selectedMaravilla!: Maravilla;
+  selected = false;
 
   constructor(private maravillaService: MaravillaService) { }
 
   getMaravillas(): void {
     this.maravillaService.getMaravillas().subscribe((maravillas) => {
       this.maravillas = maravillas;
+      this.maravillas= this.maravillas.sort((a, b) => a.country.localeCompare(b.country));
+      let newid= 1;
+      for (let m of this.maravillas){
+        m.id= newid;
+        newid += 1;
+      }
     });
+  }
+
+  onSelected(maravilla: Maravilla): void {
+    this.selected = true;
+    this.selectedMaravilla = maravilla;
   }
 
   ngOnInit() {
     this.getMaravillas();
+
   }
 
 }
